95df4d3a957f7cd7d0b0a24dcc590b03070262b1
Mohamed Ayman <mohameddallash21@gmail.com>
2023-06-18 20:33:46 +0300

    Updated game timer duration

--- server/game_server.py
+++ server/game_server.py
@@ -42,7 +42,7 @@ class GameServer:
                     reply = f"OBSTACLE: {random.randrange(80,240)}"
                     util.broadcast(message= reply, clients=self.session.game_clients)
             
-            if self.session.started and time.time() - self.session.start_time >= 10000:
+            if self.session.started and time.time() - self.session.start_time >= 60:
                 print("session time expired")
                 message = "END:"
                 util.broadcast(message, self.session.game_clients)
